ab39f361fa213dbcc05b31ceaee2450af30b6a42
Fmarius89 <44867857+Fmarius89@users.noreply.github.com>
2020-05-11 16:44:05 +0300

    Update deployment.groovy

--- deployment.groovy
+++ deployment.groovy
@@ -7,4 +7,20 @@ node("master"){
                 sh "docker rm -f nginxfrontend"
                 sh "docker run -d --name nginxfrontend -p 80:80 nginxfrontend:$BUILD_NUMBER"
         }
+       stage("Test the URL){
+                try{
+                        def output = sh script: "http://marius-lb-491722137.eu-west-1.elb.amazonaws.com/", returnStdout: true
+                        println "Output message is: "+output
+                        if(output.contains("awesome")){
+                                println "Ok i found it!!!!"
+                        }else{
+                                println "not found!!!!!! very bad image!!"
+                                sh "exit 1"
+                        }
+
+                }catch(err){
+                        println "error!!!"
+                        sh "exit 1"
+                }
+        }
 }
